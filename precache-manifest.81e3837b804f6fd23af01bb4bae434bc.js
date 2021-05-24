6fa552e928b9167e91be28faec48f1b6c3682ae5
mark <markbarla15@gmail.com>
2021-05-24 16:32:56 +0300

    Updates

---
+++ precache-manifest.81e3837b804f6fd23af01bb4bae434bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c3860b0e909b5b53dcdb54b10f50c3",
+    "url": "/adventure-site/index.html"
+  },
+  {
+    "revision": "626b95089506cad030d6",
+    "url": "/adventure-site/static/css/main.2e278bbb.chunk.css"
+  },
+  {
+    "revision": "68936d2657684db64230",
+    "url": "/adventure-site/static/js/2.d0b79010.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/adventure-site/static/js/2.d0b79010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626b95089506cad030d6",
+    "url": "/adventure-site/static/js/main.9b963568.chunk.js"
+  },
+  {
+    "revision": "56ea4b3ee7b2422173a9",
+    "url": "/adventure-site/static/js/runtime-main.2964b3f5.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/adventure-site/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/adventure-site/static/media/img-10.4143d240.jpg"
+  },
+  {
+    "revision": "369d2a847d6d7aa6ef24fc4b1a17204f",
+    "url": "/adventure-site/static/media/img-2.369d2a84.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/adventure-site/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/adventure-site/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/adventure-site/static/media/img-8.d211c298.jpg"
+  },
+  {
+    "revision": "523a89f64e53deb7aa02e9ff0fa6d18f",
+    "url": "/adventure-site/static/media/img-9.523a89f6.jpg"
+  },
+  {
+    "revision": "229f5279475d67e3cf902f7ab5cd818b",
+    "url": "/adventure-site/static/media/video-1.229f5279.mp4"
+  }
+]);
